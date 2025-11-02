30e898daaa2463070dabd9e63d5a465741d5efd0
Mohamed shaban <moha2023toedu@gmail.com>
2025-11-02 10:21:35 +0200

    add update project

--- weekly Project/app.js
+++ weekly Project/app.js
@@ -1,33 +1,43 @@
-// app.js
-const os =require('os')
-
-const platform=os.platform()
-const totalMem=os.totalmem()
-const freeMem=os.freemem()
-console.log(`Platform: ${platform}`)
-console.log(`totalMem: ${totalMem}`)
-console.log(`freeMem : ${freeMem}`)
-
+const os=require('os')
 const fs=require('fs')
+const http=require('http')
+const {celsiusToFahrenheit , calculateArea}=require('./utlis.js')
+const platform=os.platform();
+const totalMemory=os.totalmem();
+const freeMemory=os.freemem();
 
-const logData=`platform: ${platform} \n TotalMem: ${totalMem} \n ${freeMem}`
+console.log(`plateform: ${platform}`)
+console.log(`totalMemory: ${totalMemory}`)
+console.log(`freeMemory: ${freeMemory}`)
+
+const logData=`platform: ${platform}\n TotalMemory:${totalMemory}\n freeMemory:${freeMemory}`
 
 fs.writeFile('log.txt',logData,(err)=>{
-    if(err){
-        console.log(err)
-    }
-    console.log('data added successfully')
-})
-const {celsiusToFahrenheit,calculateArea}=require('./utils.js')
-const temp=celsiusToFahrenheit(30)
-const area=calculateArea(5,10)
-console.log(`30°C = ${temp}°F`);
-console.log(`Area of 5x10 = ${area}`);
-
-fs.appendFile("log.txt",`30°C = ${temp}°F \nArea of 5x10 = ${area} `,(err)=>{
-    if(err){
-        console.log(err)
-    }
-    console.log('data appended successfully')
+    if(err) console.log(err);
+    else console.log('system info saved successfully')
 })
 
+const temp=celsiusToFahrenheit(30);
+const area=calculateArea(5,10);
+
+console.log(`30c=${temp}F`);
+console.log(`Area of 5*10=${area}`);
+
+fs.appendFile('log.txt',`30c=${temp}F \n Area of 5*10=${area}`,(err)=>{
+    if(err)console.log(err)
+        else console.log('calculations append successfully')
+})
+
+http.get('http://api.open-notify.org/astros.json',(res)=>{
+    let data='';
+    res.on('data',chunck=>{data +=chunck});
+    res.on('end',()=>{
+        console.log('data fetched from API ',data)
+        fs.appendFile('log.txt',`http data:\n ${data}`,(err)=>{
+    if(err)console.log(err)
+        else console.log('http data append successfully')
+})
+    }).on('error',err=>{
+        console.log('error fetching HTTP data',err.messsage)
+    })
+})
